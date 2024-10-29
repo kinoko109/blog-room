44ce58caab7224454a38c26128336be90757b95c
kinoko109 <tatsuaki1009@gmail.com>
2024-10-29 23:34:31 +0900

    feat: health check routes

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "health_check", to: "health_check#index"
+    end
+  end
 end
