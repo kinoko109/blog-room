70be61dabff2d923b5efdcb9b3ba81f60d304bf8
kinoko109 <tatsuaki1009@gmail.com>
2024-11-04 18:57:12 +0900

    fix: db:migrate

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,5 @@
 # Rails.application.routes.draw do
+# mount_devise_token_auth_for 'User', at: 'auth'
 #   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 #   # Defines the root path route ("/")
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
